6ceb52af44cd60c2f1e7a9160473ff7dc93930a2
Christina Gislason <christinamkcho@gmail.com>
2018-11-14 12:17:11 -0800

    conflict resolution

--- InteriorShoppe/Startup.cs
+++ InteriorShoppe/Startup.cs
@@ -38,7 +38,6 @@ namespace InteriorShoppe
 
             services.AddDbContext<ApplicationDbContext>(options =>
                  options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));           
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
