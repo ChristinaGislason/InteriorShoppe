ec598d32d62d436bb72e1ffc96eacb1956214b7d
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-28 14:58:41 -0800

    created emailSender model and implenmented IEmailSender interface

---
+++ InteriorShoppe/Models/EmailSender.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InteriorShoppe.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public IConfiguration Configuration { get; }
+
+        public EmailSender(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
